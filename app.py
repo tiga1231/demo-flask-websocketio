8d8aade45c175f7e78197ade14d354aff9e8e6c7
jack <jack@email.com>
2019-09-13 11:02:59 -0700

    init

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+from flask_socketio import send, emit
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@socketio.on('message')
+def handle_message(message):
+    print('received message: ' + message)
+    send('have you said {} ? '.format(message))
+    
+# @socketio.on('json')
+# def handle_json(json):
+#     print('received json: ' + str(json))
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
+
+
