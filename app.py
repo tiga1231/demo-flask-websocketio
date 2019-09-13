10773554b591a2b9edac199ebd095e17159002dd
jack <jack@email.com>
2019-09-13 11:08:38 -0700

    readme, html content

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ def index():
 @socketio.on('message')
 def handle_message(message):
     print('received message: ' + message)
-    send('have you said {} ? '.format(message))
-    
+    send('Have you said "{}" ? '.format(message))
+
 # @socketio.on('json')
 # def handle_json(json):
 #     print('received json: ' + str(json))
---
+++ readme.md
@@ -0,0 +1,4 @@
+- ```python app.py```
+
+- open http://localhost:5000 in browser
+
